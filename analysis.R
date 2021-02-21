a08db8fc331ca475a54daa6d8ea91ac72b775026
Maddie Wong <maddiewong17@gmail.com>
2021-02-21 13:16:56 -0800

    created analysis.R file

---
+++ analysis.R
@@ -0,0 +1,54 @@
+# Assignment 3: Data Visualization (incarceration) 
+# Create an analysis.R file to analyze dataframe and think about how the
+# measures of incarceration vary by race 
+
+# Load in packages to work with data 
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+
+# Load in incarceration_trends.csv 
+Incarceration_Trends <- read.csv("https://raw.githubusercontent.com/vera-institute/incarceration-trends/master/incarceration_trends.csv")
+
+# Calculate at least five summary values from the data 
+# One
+
+# Two
+
+# Three
+
+# Four
+
+# Five
+
+# Make a chart that shows trends over time for a variable of your choice 
+# Show more than one but fewer than 10 lines. Your graph should compare the trend
+# of your measure over time. This may mean showing the same measure for different locations,
+# or different racial groups. Think careful about a meaningful comparison of locations (e.g the
+# top 10 counties in a state, top 10 states, etc.)
+# Must have clear x/y labels, a clear title, and a clear legend with different line colors
+
+
+
+# Make a chart that compares two continuous variables to one another. Think carefully
+# about what such a comparison means, and want to communicate to your user (you may have to find 
+# relevant trends in the dataset first!)
+# Must have clear x/y labels, clear title, if you choose to add color encoding (not required), you
+# need a legend for your different color and a clear legend title 
+
+
+
+# Make a map that shows how your measure of interests varies/is distributed  geographically.
+# Think carefully about what such a comparison means, and want to communicate to your user
+# Must have a title, color scale needs a legend with a clear label
+# Use a map based coordinate system to set the aspect ratio of your map (see reading) 
+# Use a minimalist theme for the map 
+
+
+
+
+
+
+
+
+
