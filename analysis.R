cc73fdb0f4bcf53140224f9a072c97f667763e5d
Maddie Wong <maddiewong17@gmail.com>
2021-02-21 16:09:09 -0800

    added most of summary stats but kinda struggling

--- analysis.R
+++ analysis.R
@@ -6,6 +6,7 @@
 library(dplyr)
 library(tidyr)
 library(ggplot2)
+library(stringr)
 
 # Load in incarceration_trends.csv & Jail Jurisdiction data 
 # Beware of missing values: You don't need to worry about "imputing" these, but
@@ -22,19 +23,62 @@ Jail_Jurisdiction <- read.csv("https://raw.githubusercontent.com/vera-institute/
 # varying numbers of incarcerated populations to look at the different trends later on 
 # specifically relating to the years after the introduction of the 1994 Crime Bill 
 
+# Calculate the total number of counties in Incarceration_Trends 
+num_counties <- Incarceration_Trends %>%
+  group_by(county_name) %>%
+  summarize(num_counties = length(unique(county_name))) %>%
+  summarize(num_counties = sum(num_counties)) %>%
+  pull(num_counties) 
+
 # What is the average amount of female inmates in 1995?
+avg_female_1995 <- Incarceration_Trends %>%
+  group_by(county_name) %>%
+  filter(year == 1995) %>%
+  summarize(female_pop_15to64 = sum(female_pop_15to64)) %>%
+  summarize(avg_female_1995 = female_pop_15to64 / num_counties) %>%
+  pull(avg_female_1995) 
 
 # What is the average amount of male inmates in 1995?
+avg_male_1995 <- Incarceration_Trends %>%
+  group_by(county_name) %>%
+  filter(year == 1995) %>%
+  summarize(male_pop_15to64 = sum(male_pop_15to64)) %>%
+  summarize(avg_male_1995 = male_pop_15to64 / num_counties) %>%
+  pull(avg_male_1995)
 
 # What is the proportion of total inmates from 1993 compared to 1995 after the 1994 Crime Bill?
+# Calculate total inmate population in 1993 
+total_1993 <- Incarceration_Trends %>%
+  filter(year == 1993) %>%
+  summarize(total_pop = sum(total_pop)) %>%
+  pull(total_pop)
+
+# Calculate total inmate population in 1995 
+total_1995 <- Incarceration_Trends %>%
+  filter(year == 1995) %>%
+  summarize(total_pop = sum(total_pop)) %>%
+  pull(total_pop)
+
+# Calculate proportion of 1993 inmates / 1995 inmates to see the difference after 1994 Crime Bill
+prop_1993_1994 <- (total_1993 / total_1995)
 
 # What is the county with the most black inmates in 1995?
+county_most_black_inmates <- Incarceration_Trends %>%
+  group_by(year) %>%
+  filter(year == 1995) %>%
+  summarize(black_pop_15to64 = max(black_pop_15to64)) %>%
+  pull(county_name)
 
 # What is the county with the least black inmates in 1995?
+county_least_black_inmates <- Incarceration_Trends %>%
+  group_by(year) %>%
+  filter(year == 1995) %>%
+  summarize(black_pop_15to64 = min(black_pop_15to64)) %>%
+  pull(county_name)
 
-# What was the highest overall incarcerated population before 1994?
+# What year and total was the highest overall incarcerated population before 1994?
 
-# What was the highest overall incarcerated population after 1994? 
+# What year and total was the highest overall incarcerated population after 1994? 
 
 # Make a chart that shows trends over time for a variable of your choice 
 # Show more than one but fewer than 10 lines. Your graph should compare the trend
