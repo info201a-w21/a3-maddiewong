985bc2bf392cec625d3b2514eb78ca876abd7d79
Maddie Wong <maddiewong17@gmail.com>
2021-02-22 16:08:09 -0800

    added charts to index

--- analysis.R
+++ analysis.R
@@ -14,9 +14,11 @@ library(mapproj)
 # when choosing what to visualize, you will want to focus on a particular
 # location and/or year that has sufficient data (note, this varies across variables)
 
-Incarceration_Trends <- read.csv("https://raw.githubusercontent.com/vera-institute/incarceration-trends/master/incarceration_trends.csv")
+Incarceration_Trends <- read.csv("https://raw.githubusercontent.com/vera-institute/
+                                 incarceration-trends/master/incarceration_trends.csv")
 
-Jail_Jurisdiction <- read.csv("https://raw.githubusercontent.com/vera-institute/incarceration-trends/master/incarceration_trends_jail_jurisdiction.csv")
+Jail_Jurisdiction <- read.csv("https://raw.githubusercontent.com/vera-institute/
+                        incarceration-trends/master/incarceration_trends_jail_jurisdiction.csv")
 
 # Calculate at least five summary values from the data --  
 # Summary stats chosen relate to the differences in genders for inmates (ages 15-64) as well as 
@@ -64,18 +66,6 @@ summary_info$total_1995 <- Incarceration_Trends %>%
 # Calculate proportion of 1993 inmates / 1995 inmates to see the difference after 1994 Crime Bill
 summary_info$prop_1993_1995 <- (total_1993 / total_1995)
 
-# What is the county with the most Black inmates ages 15 to 64 in 1995?
-summary_info$county_most_black_inmates <- Incarceration_Trends %>%
-  filter(year == 1995) %>%
-  summarize(black_pop_15to64 = max(black_pop_15to64)) %>%
-  pull(county_name)
-
-# What is the county with the least Black inmates ages 15 to 64 in 1995?
-summary_info$county_least_black_inmates <- Incarceration_Trends %>%
-  filter(year == 1995) %>%
-  summarize(black_pop_15to64 = min(black_pop_15to64)) %>%
-  select(county_name)
-
 # What was the highest overall incarcerated population before 1994?
 summary_info$highest_pre1994 <- Incarceration_Trends %>%
   filter(year < 1994) %>%
@@ -108,11 +98,11 @@ trends_data <- Incarceration_Trends %>%
 # Aggregate the trends data frame to consolidate totals by year 
 trends_totals <- aggregate(trends_data['black_pop_15to64'], by = trends_data['year'], sum)
   
-# Make a trends bar chart displaying the changes in the amount of Black inmates from 1994 to 2003 
+# Make a trends line chart displaying the changes in the amount of Black inmates from 1994 to 2003 
 # in California to show changes after the implementation of the 1994 Crime Bill 
 trends_chart <- ggplot(data = trends_totals) +
-  geom_bar(mapping = aes(x = year, y = black_pop_15to64), stat = "count") +
-  labs(x = "Year", y = "Number of Black Inmates", 
+  geom_line(mapping = aes(x = year, y = black_pop_15to64)) +
+  labs(x = "Year", y = "Number of Black Inmates",
     title = "Population of Black Inmates in California Jails from 1993 to 2003")
 
 plot(trends_chart) # Plot trends_chart 
@@ -128,20 +118,22 @@ continuous_data <- Incarceration_Trends %>%
   group_by(year) %>%
   filter(state == "CA") %>%
   filter(year > 1993 && year <= 2003) %>%
-  select(year, state, county_name, black_pop_15to64, white_pop_15to64) 
+  mutate(poc_pop = (black_pop_15to64 + latinx_pop_15to64 + native_pop_15to64)) %>%
+  select(year, state, county_name, poc_pop, white_pop_15to64) 
 
-# Aggregate the data for total white inmate population 
+# Aggregate the data for total POC inmate population 
 white_inmate_total <- aggregate(continuous_data['white_pop_15to64'], by = trends_data['year'], sum)
 
 # Aggregate the data for total black inmate population 
-black_inmate_total <- aggregate(continuous_data['black_pop_15to64'], by = trends_data['year'], sum)
+poc_inmate_total <- aggregate(continuous_data['poc_pop'], by = trends_data['year'], sum)
 
-# Create a chart to compare the populations of black vs. white inmates from 1994 to 2003 
-continuous_chart <- ggplot() +
-  geom_line(data = white_inmate_total, aes(x = year, y = white_pop_15to64), color = "blue") +
-  labs(x = "Year", y = "Number of Inmates") +
-  geom_line(data = black_inmate_total, aes(x = year, y = black_pop_15to64), color = "yellow") +
-  labs(x = "Year", y = "Number of  Inmates")
+# Join inmate totals 
+inmate_total_pop <- left_join(white_inmate_total, poc_inmate_total, by = "year")
+
+# Create a chart to compare the populations of black vs. white inmates in California from 1994 to 2003 
+continuous_chart <- ggplot(data = inmate_total_pop) +
+  geom_point(mapping = aes(x = white_pop_15to64, y = poc_pop), color = "blue") +
+  labs(x = "Number of White Inmates", y = "Number of POC Inmates") 
 
 plot(continuous_chart) # Plot continuous_chart 
 
@@ -153,15 +145,19 @@ plot(continuous_chart) # Plot continuous_chart
 # data is present/absent
 
 # Load in data for state codes 
-state_codes <- read.csv("https://raw.githubusercontent.com/info201a-w21/a3-maddiewong/main/state_codes.csv?token=ASLHHCLZ72QZ67HUUNCQG73AHVJPG")
+state_codes <- read.csv("https://raw.githubusercontent.com/info201a-w21/a3-maddiewong
+                        /main/state_codes.csv?token=ASLHHCLZ72QZ67HUUNCQG73AHVJPG")
 
 # Mutate incarceration data for map chart using data from 1994-2003 to see changes in 
 # incarceration populations 
 maps_data <- Incarceration_Trends %>%
   group_by(year) %>%
   filter(year > 1993 && year <= 2003) %>%
-  # Add in mutate() function
-  select(year, state, county_name, total_pop_15to64)
+  select(year, Code = state, county_name, total_pop_15to64)
+
+# Join maps_data and state codes 
+join_map_codes <- left_join(maps_data, state_codes, by = "Code") %>%
+  mutate(region = tolower(State)) # Mutate state to lower to combine w/ map shape later 
   
 # Define a minimalist theme 
 map_theme <- theme_bw() +
@@ -175,17 +171,18 @@ map_theme <- theme_bw() +
 
 # Load in shape of states 
 map_shape <- map_data("state") %>%
-  left_join(maps_data, by = "region")
+  left_join(join_map_codes, by = "region")
 
 # Create a blank map of states 
-map_chart <- ggplot(map_shape) %>%
+map_chart <- ggplot(data = map_shape) %>%
   geom_polygon(
     mapping = aes(x = long, y = lat, group = group, fill = total_pop_15to64),
     color = "blue",
     size = .1
-  )
+  ) +
+  coord_map() 
 
-
+plot(map_chart) # Plot map_chart 
 
 
 
